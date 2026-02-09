4fee10e3cef96dd1fc936d93a227fabc1dc81044
GDAcourse <sandyeditor37@gmail.com>
2026-02-09 16:57:09 +0530

    app.js

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwi1Gh-G1jpxM-me1iM0hdbtGoNsoZT2Mchyf795tAqmjcZEAoIi4sVzhWCx7H9WDZX/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzlUu8zmtpvEvnQPX8EytVRKQA_uiYP7aWuxavxO9JxzEcFmH8cr7X4NjsiLc__gsUo/exec";
 
-// Stock Entry
 function saveStockEntry() {
   const data = {
-    type: "stockEntry",
     date: document.getElementById("date").value,
     item: document.getElementById("item").value,
     category: document.getElementById("category").value,
     quantity: document.getElementById("quantity").value
   };
 
-  sendData(data);
-}
-
-// Stock Used
-function saveStockUsed() {
-  const data = {
-    type: "stockUsed",
-    date: document.getElementById("u_date").value,
-    item: document.getElementById("u_item").value,
-    quantity: document.getElementById("u_quantity").value
-  };
-
-  sendData(data);
-}
-
-function sendData(data) {
   fetch(API_URL, {
     method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" }
+    body: JSON.stringify(data)
   })
-  .then(res => res.text())
-  .then(msg => {
-    document.getElementById("msg").innerText = msg + " ✅";
+  .then(res => res.json())
+  .then(res => {
+    alert("Stock Entry Saved Successfully");
   })
   .catch(err => {
-    document.getElementById("msg").innerText = "Error ❌";
-    console.error(err);
+    alert("Error saving data");
   });
 }
