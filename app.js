7f0f2c936b60b2af349766d998bad25bfd1e9613
GDAcourse <sandyeditor37@gmail.com>
2026-02-10 14:49:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwB9lIzkd0pU0AOg9gMAuW78nq1K8D2V9zM07CSFxpref8OYVw2vG1aeHtnYtXkV6ZU/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxcyX9v4CoA4iVsG85mOb7xOnNpUuvB6x7Bo-D2ja9JjkP5GxW1ULU9-iUYXIBthbUl/exec";
 
 function saveStockEntry() {
   const data = {
@@ -17,3 +17,20 @@ function saveStockEntry() {
   .then(txt => alert(txt))
   .catch(err => alert("Error"));
 }
+function saveStockUsed() {
+  const data = {
+    type: "Stock Used",
+    date: document.getElementById("used_date").value,
+    item: document.getElementById("used_item").value,
+    quantity: document.getElementById("used_quantity").value
+  };
+
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify(data)
+  })
+  .then(res => res.text())
+  .then(txt => alert(txt))
+  .catch(err => alert("Error"));
+}
+
