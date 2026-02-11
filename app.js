fca4f4155803c8321d2438b42e1ff14a63733f9c
GDAcourse <sandyeditor37@gmail.com>
2026-02-11 15:01:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbw4D5a0VfouI5aRR9H12zhZnofhrVFZZ59WrOrEtf5JBhFSWKrgqI3_uNucAa3p2ePP/exec";
+const ENTRY_API = "https://script.google.com/macros/s/AKfycbw4D5a0VfouI5aRR9H12zhZnofhrVFZZ59WrOrEtf5JBhFSWKrgqI3_uNucAa3p2ePP/exec";
+const USED_API = "https://script.google.com/macros/s/AKfycbyOUvFvhmKNNxooDytJu3D9zIoH0MN1uavFe5fBaI0mHB6TOvEZXe9yrEw1knnQOThSnw/exec";
 
 function saveStockEntry() {
 
@@ -7,7 +8,7 @@ function saveStockEntry() {
   const category = document.getElementById("entryCategory").value;
   const quantity = document.getElementById("entryQuantity").value;
 
-  fetch(API_URL, {
+  fetch(ENTRY_API, {
     method: "POST",
     body: JSON.stringify({
       date: date,
@@ -17,10 +18,28 @@ function saveStockEntry() {
     })
   })
   .then(res => res.text())
-  .then(data => {
-    alert(data);
+  .then(data => alert(data))
+  .catch(err => alert("Network Error"));
+}
+
+
+function saveStockUsed() {
+
+  const date = document.getElementById("usedDate").value;
+  const item = document.getElementById("usedItem").value;
+  const category = document.getElementById("usedCategory").value;
+  const quantity = document.getElementById("usedQuantity").value;
+
+  fetch(USED_API, {
+    method: "POST",
+    body: JSON.stringify({
+      date: date,
+      item: item,
+      category: category,
+      quantity: quantity
+    })
   })
-  .catch(err => {
-    alert("Network Error");
-  });
+  .then(res => res.text())
+  .then(data => alert(data))
+  .catch(err => alert("Network Error"));
 }
